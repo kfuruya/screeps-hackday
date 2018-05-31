bba11cb8683e9a3bc766a14000a7fe170c273684
Kim Furuya <3.14kfuruya159@gmail.com>
2018-05-31 12:45:01 -0700

    mv role harvester to its own init script

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 var roleHarvester = require('role.harvester');
-
+roleHarvester.init();
 
 module.exports.loop = function () {
-    Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'harvester1' )
     for(var name in Game.creeps) {
         console.log("test");
         var creep = Game.creeps[name];
--- role.harvester.js
+++ role.harvester.js
@@ -1,19 +1,27 @@
-var roleHarvester = {
-
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
+names = require('./utils/names');
+/** @param {Creep} creep **/
+function run(creep) {
+    if(creep.carry.energy < creep.carryCapacity) {
+        var sources = creep.room.find(FIND_SOURCES);
+        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(sources[0]);
         }
-        else if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
-            if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1']);
-            }
+    }
+    else if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
+        if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(Game.spawns['Spawn1']);
         }
-	}
+    }
 };
 
-module.exports = roleHarvester;
+function init(name, spawn) {
+    if (!spawn) {
+        spawn = 'Spawn1';
+    }
+    if (!name) {
+        name = names[Math.floor(Math.random() * names.length)] + names[Math.floor(Math.random() * names.length)];
+    }
+    Game.spawns[spawn].spawnCreep( [WORK, CARRY, MOVE], name )
+}
+
+module.exports = {run, init};
