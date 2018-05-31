10549127826cc93d6af89d51c104151b85073afb
abhi <ahada2@meredith.com>
2018-05-31 11:51:28 -0700

    Renaming to correct

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var roleHarvester = require('role.harvester');
 
-module.exports.loop = function () {
 
+module.exports.loop = function () {
+    Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'harvester1' )
     for(var name in Game.creeps) {
+        console.log("test");
         var creep = Game.creeps[name];
         roleHarvester.run(creep);
     }
